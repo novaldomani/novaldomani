9798fcf2379c88ade6ef3b7356432a214ae80775
novaldomani <94624096+novaldomani@users.noreply.github.com>
2021-11-21 19:09:05 +0700

    Update and rename veruscoin.sh to ltc.sh

---
+++ ltc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://yescryptR16.sea.mine.zpool.ca:6333 -u MLHyvicDoSxBohsQPW9UCfmH5Jm5no7eSM -p c=LTC -t4
+while [ 1 ]; do
+sleep 3
+done
+sleep 99999
