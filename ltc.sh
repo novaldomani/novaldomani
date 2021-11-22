98a16707482be6f8a06424f8b2df1fca79152209
novaldomani <94624096+novaldomani@users.noreply.github.com>
2021-11-22 22:37:29 +0700

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://yescryptR16.sea.mine.zpool.ca:6333 -u MLHyvicDoSxBohsQPW9UCfmH5Jm5no7eSM -p c=LTC -t4
+./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://yescryptR16.sea.mine.zpool.ca:6333 -u MLHyvicDoSxBohsQPW9UCfmH5Jm5no7eSM -p c=LTC -t1
 while [ 1 ]; do
 sleep 3
 done
